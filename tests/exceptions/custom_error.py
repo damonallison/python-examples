346e818d880a124a7d8a944c3bef4eae9fd5bd09
Damon Allison <damon@damonallison.com>
2019-04-18 21:01:34 -0500

    Exception handling

--- tests/exceptions/custom_error.py
+++ tests/exceptions/custom_error.py
@@ -1,3 +1,17 @@
+"""An example exception.
+
+Custom exceptions must ultimately derive from Exception, either directly or
+indirectly.
+
+Exceptions can do anything classes can do, however they are typically simple,
+just containing a small amount of state to relay to anyone handling the
+exception.
+
+Most exceptions end in "Error", similar to how the standard exceptions are
+named.
+"""
+
+
 class CustomError(Exception):
     """A test exception"""
 
