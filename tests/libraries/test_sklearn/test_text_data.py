2bef4d4cb2c8b8004268d7185769ac9f90004276
Damon Allison <damon@shipt.com>
2022-05-11 07:30:29 -0500

    Text feature engineering w/ BoW and tf-idf.

--- tests/libraries/test_sklearn/test_text_data.py
+++ tests/libraries/test_sklearn/test_text_data.py
@@ -43,7 +43,13 @@ import os
 
 import numpy as np
 import pandas as pd
-from sklearn import datasets, feature_extraction, linear_model, model_selection
+from sklearn import (
+    datasets,
+    feature_extraction,
+    linear_model,
+    model_selection,
+    pipeline,
+)
 
 
 def load_data(dir: str) -> tuple[list[str], list[str], list[str], list[str]]:
@@ -92,7 +98,17 @@ def test_bag_of_words() -> None:
         "but the wise man knows himself to be a fool",
     ]
 
+    #
     # CountVectorizer tokenizes the training data and builds the vocabulary.
+    #
+    # Each word becomes a key, it's ordinal value is the value
+    #
+    # {
+    #   "the": 10,
+    #   "fool": 4,
+    #   ...
+    # }
+    #
     vect = feature_extraction.text.CountVectorizer()
     vect.fit(corpus)
 
@@ -109,9 +125,11 @@ def test_bag_of_words() -> None:
     df = pd.DataFrame(bow.toarray(), columns=vect.get_feature_names())
     print(df)
 
+    #
     # Words that appear in a single document are unlikely to be in our test set.
     # To eliminate these words, only include words that appear in `n` or more
     # documents using the `min_df` parameter.
+    #
     vect = feature_extraction.text.CountVectorizer(min_df=2)
     vect.fit(corpus)
 
@@ -121,27 +139,220 @@ def test_bag_of_words() -> None:
 
 
 def test_bow_movie_reviews() -> None:
-    X_train, X_test, y_train, y_test = load_data("./data/aclImdb")
+    text_train, text_test, y_train, y_test = load_data("./data/aclImdb")
 
-    vect = feature_extraction.text.CountVectorizer(min_df=5)
-    vect.fit(X_train)
-    X_train = vect.transform(X_train)
+    vect = feature_extraction.text.CountVectorizer()
+    vect.fit(text_train)
+    X_train = vect.transform(text_train)
 
+    print(f"X_train.shape: {X_train.shape}")
+    print(f"Vocab length: {len(vect.vocabulary_)}")
+    print(f"X_train: {repr(X_train)}")
+
+    #
+    # Since we have not done any preprocessing, CountVectorizer picks up all
+    # kinds of "words", including numerics, multiple versions of the same word
+    # (draw, drawing, draws). The default regex used to split words is
+    # "\b\w\w+\b" where \b is a word boundary. \b is *not* included in the
+    # match.
+    #
+    # \b = word boundary
+    # \w = [A-Za-z0-9_]
+    #
+
+    #
     # Determine baseline performance before improving feature extraction
+    #
     scores = model_selection.cross_val_score(
         linear_model.LogisticRegression(), X_train, y_train, cv=5
     )
     baseline_cv_mean = np.mean(scores)
-    print(f"Mean cv accuracy: {baseline_cv_mean:.2f}")
+    print(f"Default mean cv accuracy: {baseline_cv_mean:.2f}")
 
+    #
     # Let's tune C to determine if we can improve on the baseline
+    #
     param_grid = {"C": [0.001, 0.01, 0.1, 1, 10]}
     grid = model_selection.GridSearchCV(
         linear_model.LogisticRegression(), param_grid, cv=5
     )
     grid.fit(X_train, y_train)
-    print(f"Best cv score: {grid.best_score_}")
-    print(f"Best params: {grid.best_params_}")
+    print(f"Tuned best cv score: {grid.best_score_}")
+    print(f"Tuned best params: {grid.best_params_}")
 
-    X_test = vect.transform(X_test)
-    print(f"Test score: {grid.score(X_test, y_test):.2f}")
+    X_test = vect.transform(text_test)
+    print(f"Tuned test score: {grid.score(X_test, y_test):.2f}")
+
+    #
+    # Let's try throwing away words that are in less than 5 documents, resulting
+    # in a much smaller vocabulary (less features), to determine if model
+    # performance suffers.
+    #
+
+    vect = feature_extraction.text.CountVectorizer(min_df=5)
+    vect.fit(text_train)
+    X_train = vect.transform(text_train)
+
+    print(f"Trimmed X_train.shape: {X_train.shape}")
+    print(f"Trimmed vocab length: {len(vect.vocabulary_)}")
+    print(f"Trimmed X_train: {repr(X_train)}")
+
+    grid = model_selection.GridSearchCV(
+        linear_model.LogisticRegression(), param_grid, cv=5
+    )
+    grid.fit(X_train, y_train)
+    print(f"Trimmed mean cv accuracy: {grid.best_score_}")
+
+    #
+    # Notice we didn't reduce accuracy while cutting the vocabulary count from
+    # 3445861 to 27272. That proves that "unique" (or infrequent) words didn't
+    # matter.
+    #
+
+
+def test_feature_engineering_remove_stop_words() -> None:
+    #
+    # Stop words are too frequent to be informative. (`the`, `is`, `a`, `this`)
+    #
+    # By removing the stop words (300+ words by default), we remove *some*
+    # features but the overall test performance dropped a bit. It doesn't make
+    # sense to omit stop words with this dataset.
+    #
+    # Stop words are helpful for small data sets, which don't contain a lot of
+    # information for the model to determine which words are important.
+    #
+
+    stop_words = feature_extraction.text.ENGLISH_STOP_WORDS
+
+    text_train, text_test, y_train, y_test = load_data("./data/aclImdb")
+
+    vect = feature_extraction.text.CountVectorizer(min_df=5, stop_words=stop_words)
+    vect.fit(text_train)
+    X_train = vect.transform(text_train)
+
+    print(f"X_train.shape: {X_train.shape}")
+    print(f"Vocab length: {len(vect.vocabulary_)}")
+    print(f"X_train: {repr(X_train)}")
+
+    param_grid = {"C": [0.001, 0.01, 0.1, 1, 10]}
+    grid = model_selection.GridSearchCV(
+        linear_model.LogisticRegression(), param_grid, cv=5
+    )
+    grid.fit(X_train, y_train)
+    print(f"Tuned best cv score: {grid.best_score_}")
+    print(f"Tuned best params: {grid.best_params_}")
+
+    X_test = vect.transform(text_test)
+    print(f"Tuned test score: {grid.score(X_test, y_test):.2f}")
+
+
+def test_feature_engineering_tfidf() -> None:
+    #
+    # tf-idf is a way to rank features by importance (or weight).
+    #
+    # "The intuition of this method is to give high weight to any term that
+    # appears often in a particular document, but not in many documents in the
+    # corpus. If a word appears often in a particular document, but not in very
+    # many documents, it is likely to be very descriptive of the content of that
+    # document." - Introduction to Machine Learning - Chapter 7
+    #
+    # Words with a higher tf-idf score are considered more important.
+    #
+    # Because tf-idf uses statistical properties of the data, we will use a
+    # pipeline to ensure the results of our grid search are valid.
+
+    text_train, text_test, y_train, y_test = load_data("./data/aclImdb")
+
+    pipe = pipeline.Pipeline(
+        steps=[
+            ("tfidf", feature_extraction.text.TfidfVectorizer(min_df=5)),
+            ("lr", linear_model.LogisticRegression()),
+        ]
+    )
+    param_grid = {"lr__C": [0.001, 0.01, 0.1, 1, 10]}
+    grid = model_selection.GridSearchCV(pipe, param_grid, cv=5, n_jobs=-1)
+    grid.fit(text_train, y_train)
+    print(f"Best CV score: {grid.best_score_:.2f}")
+
+    #
+    # Which words did tf-idf deem important?
+    #
+    vect: feature_extraction.text.TfidfVectorizer = grid.best_estimator_.named_steps[
+        "tfidf"
+    ]
+    X_train = vect.transform(text_train)
+
+    # Find the maximum value for each of the features over the dataset.
+    max_value = X_train.max(axis=0).toarray().ravel()
+    sorted_by_tfidf = max_value.argsort()
+    feature_names = np.array(vect.get_feature_names())
+
+    #
+    # Features with low tf-idf are those that are very common across documents
+    # or are used sparingly only in longer documents.
+    #
+    # Features with high df-idf appear often in particular reviews. They contain
+    # specific information about the review.
+    #
+    print(f"Lowest tf-idf: {feature_names[sorted_by_tfidf[:20]]}")
+    print(f"Highest tf-idf: {feature_names[sorted_by_tfidf[-20:]]}")
+
+    # Which features have low idf? They appear frequently and are therefore
+    # deemed less important.
+    #
+    # As expected, low idf words are mostly stop words. However even words you
+    # think would be important for sentiment analysis like "good", "bad",
+    # "movie" are among the most frequent and less relevant.
+    sorted_by_idf = np.argsort(vect.idf_)
+    print(f"Lowest idf: {feature_names[sorted_by_idf[:100]]}")
+    print(f"Highest idf: {feature_names[sorted_by_idf[-100:]]}")
+
+    #
+    # Model coefficients: which features had the highest (most positive)
+    # influence and lowest (most negative) influence?
+    #
+    lr: linear_model.LinearRegression = grid.best_estimator_.named_steps["lr"]
+    coefs = np.array(lr.coef_)
+    # argsort returns indices that index the data in sorted order.
+    positive_coefs = np.argsort(coefs)[-20:]
+    negative_coefs = np.argsort(coefs)[:20]
+    print(f"Most positive features: {feature_names[positive_coefs]}")
+    print(f"Most negative features: {feature_names[negative_coefs]}")
+
+
+def test_bow_with_ngrams() -> None:
+    #
+    # n-grams add features for groups of words
+    #
+    # The main limitation of BoW is word order is completely discarded. "it's
+    # bad, it's not good at all" and "it's good, it's not bad at all" have hte
+    # same represntation but opposite meanings. Since words lose surrounding
+    # context.
+    #
+    # Important movie bigram examples are:
+    # 'not worth', 'well worth', 'definitely worth'
+    #
+    # You don't get that context from unigram (single word) BoW features.
+    #
+    # Note the feature space explodes with ngrams. bigrams == words^2, for
+    # example. This is going to be expensive.
+    #
+    text_train, text_test, y_train, y_test = load_data("./data/aclImdb")
+
+    vect = feature_extraction.text.CountVectorizer(ngram_range=(1, 3)).fit(text_train)
+    print(f"Vect vocab: {len(vect.vocabulary_)}")
+
+    pipe = pipeline.Pipeline(
+        steps=[
+            ("tfidf", feature_extraction.text.TfidfVectorizer(min_df=5)),
+            ("lr", linear_model.LogisticRegression()),
+        ]
+    )
+    param_grid = {
+        "tfidf__ngram_range": [(1, 1), (1, 2), (1, 3)],
+        "lr__C": [0.001, 0.01, 0.1, 1, 10],
+    }
+    grid = model_selection.GridSearchCV(pipe, param_grid, cv=5, n_jobs=-1)
+    grid.fit(text_train, y_train)
+    print(f"Best CV score: {grid.best_score_:.2f}")
+    print(f"Best params {grid.best_params_}")
