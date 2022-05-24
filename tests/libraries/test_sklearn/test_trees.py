82e2a168e9e17a91ee43375c5033bd60a07adcda
Damon Allison <damon@shipt.com>
2022-05-24 06:59:59 -0500

    RF / GB

--- tests/libraries/test_sklearn/test_trees.py
+++ tests/libraries/test_sklearn/test_trees.py
@@ -98,7 +98,7 @@ def run_dt(dt: tree.DecisionTreeClassifier, plot_tree: bool = False) -> None:
 
 
 def test_default_decision_tree() -> None:
-    run_dt(tree.DecisionTreeClassifier(random_state=SEED), plot_tree=True)
+    run_dt(tree.DecisionTreeClassifier(random_state=SEED), plot_tree=False)
 
 
 #
@@ -122,6 +122,16 @@ def test_tuned_decision_tree() -> None:
 
     dt = tree.DecisionTreeClassifier(random_state=SEED)
 
+    #
+    # How do you choose which values to hyperparameter tune?
+    #
+    # Each feature typically has patters (logarithmic, power of 2, linear).
+    # Understand how the feature is used and look at other examples.
+    #
+    # Ideally, the best parameters will be in the middle of each feature. That
+    # means you've exhausted the extremes and have no further room for
+    # improvement in that features.
+    #
     param_grid = {
         "max_depth": [1, 2, 3, 4, 5, None],
         "min_samples_split": [2, 3, 4],
@@ -131,13 +141,14 @@ def test_tuned_decision_tree() -> None:
         param_grid,
         cv=10,
         return_train_score=True,
+        verbose=2,
         refit=True,
     )
     grid_search.fit(X_train, y_train)
     logger.info(f"Best parameters: {grid_search.best_params_}")
-    logger.info("Best cross-validation score: {:.3f}".format(grid_search.best_score_))
+    logger.info(f"Best cross-validation score: {grid_search.best_score_:.3f}")
     logger.info(f"Accuracy on training set: {grid_search.score(X_train, y_train):.3f}")
-    logger.info(f"Test score: {grid_search.score(X_test, y_test):.3f}")
+    logger.info(f"Accuracy on test set: {grid_search.score(X_test, y_test):.3f}")
 
 
 #
@@ -153,7 +164,7 @@ def test_random_forest() -> None:
         X, y, stratify=y, test_size=0.25, random_state=SEED
     )
 
-    rf = ensemble.RandomForestClassifier(random_state=SEED)
+    rf = ensemble.RandomForestClassifier(n_jobs=-1, verbose=2, random_state=SEED)
 
     rf.fit(X_train, y_train)
 
@@ -163,7 +174,7 @@ def test_random_forest() -> None:
     feature_imp = pd.DataFrame([rf.feature_importances_], columns=rf.feature_names_in_)
     logger.info(f"Tree feature_importances: {feature_imp}")
 
-    plot_tree = True
+    plot_tree = False
     if plot_tree:
         # Plot tree
         plt.figure(figsize=(20, 10))
@@ -173,10 +184,16 @@ def test_random_forest() -> None:
         tree.plot_tree(rf.estimators_[4], feature_names=list(cancer.feature_names))
         plt.show()
 
+        #
         # Feature importances are aggregared over all trees in the forest. In a
         # similar fashion to how a forest is typically stronger than a single
         # tree, feature_importances of the forest are more reliable than an
         # individual tree importances.
+        #
+        # THe randomness in the trees forces the algorithm to consider more
+        # combinations, which captures a more complete picture of feature
+        # importances than a single tree.
+        #
         n_features = len(cancer.feature_names)
         plt.barh(np.arange(n_features), rf.feature_importances_, align="center")
         plt.yticks(np.arange(n_features), cancer.feature_names)
@@ -200,29 +217,143 @@ def test_tuned_random_forest() -> None:
     # n_samples) and "tree" level parameters (max_depth, min_samples_split,
     # max_features).
     param_grid = {
+        #
         # n_estimators: larger is always better, however there are diminishing
-        # returns.
+        # returns (and obviously higher compute cost).
+        #
         "n_estimators": [100, 500, 1000, 2000],
         #
         # Inject randomness into the "random forest"
         #
-        # max_samples: the amount of data each tree is trained on
-        # max_features: the number of features considered at each split
+        # max_samples: the amount of data each tree is trained on. None ==
+        # entire training set
+        #
         "max_samples": [100, 200, None],
         #
-        # A lower max_features == more random trees and reduces overfitting
+        # max_features: the number of features considered at each split
+        #
+        # A lower max_features == more random trees, which reduces overfitting
         # Typically, use "sqrt for classification and n_features for regression.
-        "max_features": [1, 2, len(X.columns)],
+        #
+        "max_features": [1, 2, "sqrt", len(X.columns)],
     }
     grid_search = model_selection.GridSearchCV(
         rf,
         param_grid,
-        return_train_score=True,
-        refit=True,
-        n_jobs=-1,
+        cv=5,  # The number of folds to use for cross validation (default=5)
+        return_train_score=True,  # I
+        refit=True,  # Refits an estimator using the best found parameters on the entire training set.
+        n_jobs=-1,  # Parallelize over all CPUs. Random forest builds each tree independently and is parallelizable.
         verbose=2,
     )
     grid_search.fit(X_train, y_train)
-    logger.info("Best parameters: {}".format(grid_search.best_params_))
-    logger.info("Best cross-validation score: {:.2f}".format(grid_search.best_score_))
-    logger.info("Test score: {:.2f}".format(grid_search.score(X_test, y_test)))
+    logger.info(f"Best parameters: {(grid_search.best_params_)}")
+    logger.info(f"Best cross-validation score: {grid_search.best_score_:.3f}")
+    logger.info(f"Accuracy on training set: {grid_search.score(X_train, y_train):.3f}")
+    logger.info(f"Accuracy on test set: {grid_search.score(X_test, y_test):.3f}")
+
+
+def test_gradient_boosting() -> None:
+    cancer = datasets.load_breast_cancer()
+    X = pd.DataFrame(cancer.data, columns=cancer.feature_names)
+    y: np.ndarray = cancer.target
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(
+        X, y, stratify=y, test_size=0.25, random_state=SEED
+    )
+
+    gb = ensemble.GradientBoostingClassifier(verbose=2, random_state=SEED)
+
+    gb.fit(X_train, y_train)
+    logger.info(f"Accuracy on training set: {gb.score(X_train, y_train):.3f}")
+    logger.info(f"Accuracy on test set: {gb.score(X_test, y_test):.3f}")
+
+    feature_imp = pd.DataFrame([gb.feature_importances_], columns=gb.feature_names_in_)
+    logger.info(f"Tree feature_importances: {feature_imp}")
+
+    plot_results = True
+    if plot_results:
+        #
+        # Feature importances are aggregared over all trees in the forest. In a
+        # similar fashion to how a forest is typically stronger than a single
+        # tree, feature_importances of the forest are more reliable than an
+        # individual tree importances.
+        #
+        # THe randomness in the trees forces the algorithm to consider more
+        # combinations, which captures a more complete picture of feature
+        # importances than a single tree.
+        #
+        n_features = len(cancer.feature_names)
+        plt.barh(np.arange(n_features), gb.feature_importances_, align="center")
+        plt.yticks(np.arange(n_features), cancer.feature_names)
+        plt.xlabel("feature importance")
+        plt.ylabel("feature")
+        plt.ylim(-1, n_features)
+        plt.show()
+
+
+def test_tuned_gradient_boosting() -> None:
+    cancer = datasets.load_breast_cancer()
+    X = pd.DataFrame(cancer.data, columns=cancer.feature_names)
+    y: np.ndarray = cancer.target
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(
+        X, y, stratify=y, test_size=0.25, random_state=SEED
+    )
+
+    gb = ensemble.GradientBoostingClassifier(random_state=SEED)
+
+    # Note the combination of "forest" level parameters (n_estimators,
+    # n_samples) and "tree" level parameters (max_depth, min_samples_split,
+    # max_features).
+    param_grid = {
+        #
+        # n_estimators: larger is always better, however there are diminishing
+        # returns (and obviously higher compute cost).
+        #
+        "n_estimators": [100, 500, 1000, 2000],
+        #
+        # max_depth of the individual estimators. 1 == decision stump (one decision)
+        #
+        "max_depth": [1, 2, 3],
+        #
+        # learning_rate
+        #
+        # Shrinks the contribution made by each tree. It has an inverse
+        # relationship to n_estimators. Generally, the lower the learning rate,
+        # the more n_estimators you need.
+        #
+        "learning_rate": [0.01, 0.1, 1],
+        # max_features: the number of features considered at each split
+        #
+        # A lower max_features == more random trees, which reduces overfitting.
+        #
+        # Typically, use "sqrt for classification and n_features for regression.
+        #
+        # "max_features": [1, 2, "sqrt", len(X.columns)],
+    }
+    grid_search = model_selection.GridSearchCV(
+        gb,
+        param_grid,
+        cv=5,  # The number of folds to use for cross validation (default=5)
+        return_train_score=True,  # I
+        refit=True,  # Refits an estimator using the best found parameters on the entire training set.
+        n_jobs=-1,  # Parallelize over all CPUs. Random forest builds each tree independently and is parallelizable.
+        verbose=2,
+    )
+    grid_search.fit(X_train, y_train)
+    logger.info(f"Best parameters: {(grid_search.best_params_)}")
+    logger.info(f"Best cross-validation score: {grid_search.best_score_:.3f}")
+    logger.info(f"Accuracy on training set: {grid_search.score(X_train, y_train):.3f}")
+    logger.info(f"Accuracy on test set: {grid_search.score(X_test, y_test):.3f}")
+    plot_results = True
+    if plot_results:
+        n_features = len(cancer.feature_names)
+        plt.barh(
+            np.arange(n_features),
+            grid_search.best_estimator_.feature_importances_,
+            align="center",
+        )
+        plt.yticks(np.arange(n_features), cancer.feature_names)
+        plt.xlabel("feature importance")
+        plt.ylabel("feature")
+        plt.ylim(-1, n_features)
+        plt.show()
