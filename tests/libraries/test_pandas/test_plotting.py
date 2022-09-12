dda1a46afffbf6906c9a637927febcab7de3f050
Damon Allison <damon@shipt.com>
2022-09-12 08:22:16 -0500

    Plotting and visualization

--- tests/libraries/test_pandas/test_plotting.py
+++ tests/libraries/test_pandas/test_plotting.py
@@ -3,8 +3,20 @@ import pandas as pd
 import pytest
 
 
-@pytest.mark.skip(reason="Displays a plot")
+# Mark all tests this module as 'plot'. These tests will be skipped with `make
+# test` since they block when GUI rendering.
+pytestmark = pytest.mark.plot
+
+
 def test_hist() -> None:
-    df = pd.DataFrame({"one": [0, 0.5, 1, 2, 2.5, 3, 4, 5, 6]})
-    df.hist(bins=2)
+    df = pd.DataFrame(
+        {
+            "one": [0, 0.5, 1, 2, 2.5, 3, 4, 5, 6, 7],
+            "two": [0, 1, 2, 4, 5, 8, 10, 13, 15, 20],
+        }
+    )
+    df.plot.line()
+    df.plot.scatter(x="one", y="two")
+    df.plot.bar(x="one", y="two")
+
     plt.show()
