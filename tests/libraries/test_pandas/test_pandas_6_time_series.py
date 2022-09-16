6a2baaaf62e17ac4017506458d7eb7bbf408b5fc
Damon Allison <damon@shipt.com>
2022-09-16 08:20:12 -0500

    Time series / window functions

--- tests/libraries/test_pandas/test_pandas_6_time_series.py
+++ tests/libraries/test_pandas/test_pandas_6_time_series.py
@@ -1,17 +1,29 @@
 """
 A timeseries in pandas is a Series or DataFrame that has a DateTimeIndex.
 
-* What is the relationship between `datetime`, numpy's `datetime64`, and pandas Timestamp?
+* What is the relationship between `datetime`, numpy's `datetime64`, and pandas
+  Timestamp?
 *
 * datetime does not have nanosecond precision
 * datetime64 has something strange w/ timezone handling
 * pd.Timestamp has nanosecond precision and can be made timezone aware
+
+Pandas supports "Periods", which allow you to create and aggregate data by fixed
+periods. Pandas supports accounting and fiscal year periods like "A-SEP"
+(annual, starting in september), wihch helps accounting and fiscal year /
+quarter analysis.
+
+Pandas supports rolling window functions (rolling and expanding).
+
 """
 import datetime
+import math
 import numpy as np
 import pandas as pd
 import pandas.api.types as pd_types
 import pytz
+
+import matplotlib.pyplot as plt
 
 
 def test_basics() -> None:
@@ -56,7 +68,6 @@ def test_timezone_handling() -> None:
     # Use tz_localize to convert tz naive timestamps to a tz
 
     ts = pd.date_range("2022-09-01", periods=10)
-
     assert ts.tz is None
 
     ts = ts.tz_localize("UTC")
@@ -64,3 +75,65 @@ def test_timezone_handling() -> None:
 
     ts = ts.tz_convert("America/New_York")
     assert ts.tz == pytz.timezone("America/New_York")
+
+    s = pd.Series(np.random.standard_normal(len(ts)), index=ts)
+
+    # If two time series with different TZs are combined, the result will be
+    # UTC. Pandas stores Timestamp values as UTC under the hood.
+    s1 = s[:5].tz_convert("America/Chicago")
+    s2 = s[5:]
+    assert (s1 + s2).index.tz == pytz.UTC
+
+
+def test_periods() -> None:
+    # Periods represent time spans.
+    #
+    # Periods are meant for accounting-like calculations which reside on fiscal
+    # years and quarters which may not line up with calendar year / quarters.
+
+    pi = pd.period_range("2022-09-01", "2022-12-31", freq="D")
+    assert pd_types.is_period_dtype(pi)
+
+    # Resampling is the process of converting a time series to a different
+    # frequency. Moving to a lower frequency (say day -> month) is called
+    # downsampling. Higher, upsampling.
+
+    # Downsampling
+    s = pd.Series(np.random.standard_normal(len(pi)), index=pi)
+    sm = s.resample(rule="M", kind="period").mean()
+
+    assert len(sm) == 4
+    assert (
+        sm.index.to_list()
+        == pd.period_range("2022-09-01", "2022-12-31", freq="M").to_list()
+    )
+
+    # Upsampling requires interpolation to fill in the missing gaps.
+    # This example uses ffill() (forward fill) to interpolate.
+
+    sd = sm.resample(rule="D", kind="period").ffill()
+    assert (sd["2022-09-01":"2022-09-30"] == sd["2022-09-01"]).all()
+    assert (sd["2022-10-01":"2022-10-31"] == sd["2022-10-01"]).all()
+
+
+def test_window_functions() -> None:
+
+    # 3 hours
+    s = pd.Series(
+        np.random.standard_normal(10800),
+        pd.date_range("2022-09-01", periods=10800, freq="1s"),
+    )
+
+    # Notice the first 249 data points are nan
+    s_rolling = s.rolling(250).mean()
+    assert pd.isnull(s_rolling.iloc[0:249]).all()
+
+    assert math.isclose(s_rolling.iloc[249], s.iloc[0:250].mean())
+
+    # .expanding() will start the tine window from the same point as the series
+    # and increase the size of the window until it encompasses the whole series.
+
+    s_expanding = s.expanding().mean()
+
+    for i in range(len(s)):
+        assert math.isclose(s_expanding.iloc[i], s.iloc[0 : i + 1].mean())
