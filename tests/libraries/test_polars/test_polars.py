62a895a1c243cf0f0b5794a68dbe48591ebda5d3
Damon Allison <damon@damonallison.com>
2023-10-03 20:10:44 -0500

    Adds polars / onnx

---
+++ tests/libraries/test_polars/test_polars.py
@@ -0,0 +1,15 @@
+"""Polars is a DF library written in Rust which uses Apache Arrow as it's foundation"""
+
+import polars as pl
+
+
+def test_polars_create() -> None:
+    s = pl.Series("a", [1, 2, 3, 4])
+
+    assert s.shape == (4,)
+    assert s.dtype == pl.Int64
+
+    df = pl.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6]})
+
+    assert df.columns == ["one", "two"]
+    assert df.get_column("one").series_equal(pl.Series("one", [1, 2, 3]))
