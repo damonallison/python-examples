63a4133902c05fa1954834c7092440d7d0198f91
Damon Allison <damon@shipt.com>
2024-05-31 09:53:55 -0500

    week 1: C

--- edu/harvard/cs50/week1/hello.c
+++ edu/harvard/cs50/week1/hello.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include "cs50.h"
 
+// int main(void)
+// {
+//     string s = get_string("what's your name? ");
+//     printf("hello, %s\n", s);
+// }
+
+// int main(void)
+// {
+//     char c = get_char("do you agree? ");
+
+//     // strings use "", chars use ''
+//     if (c == 'Y' || c == 'y')
+//     {
+//         printf("agreed\n");
+//     }
+//     else if (c == 'N' || c == 'n')
+//     {
+//         printf("not agreed\n");
+//     }
+//     else
+//     {
+//         printf("unknown\n");
+//     }
+// }
+
+void meow(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("meow\n");
+    }
+}
+
 int main(void)
 {
-    string s = get_string("what's your name? ");
-    printf("hello, %s\n", s);
+    meow(3);
 }
