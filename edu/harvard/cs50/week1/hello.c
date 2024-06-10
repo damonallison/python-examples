d168c51df83ab232093621ee35d0873d52dc15d2
Damon Allison <damon@shipt.com>
2024-06-10 07:29:28 -0500

    CS50 week 1

--- edu/harvard/cs50/week1/hello.c
+++ edu/harvard/cs50/week1/hello.c
@@ -34,7 +34,44 @@ void meow(int n)
     }
 }
 
+void mario(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n; j < n; j++)
+        {
+        }
+    }
+}
+int change(int n)
+{
+    int coins = 0;
+    while (n > 0)
+    {
+        if (n >= 25)
+        {
+            n -= 25;
+        }
+        else if (n >= 10)
+        {
+            n -= 10;
+        }
+        else if (n >= 5)
+        {
+            n -= 5;
+        }
+        else
+        {
+            n--;
+        }
+        coins++;
+    }
+    return coins;
+}
+
 int main(void)
 {
-    meow(3);
+    // meow(3);
+    int coins = change(80);
+    printf("you need %d coins", coins);
 }
